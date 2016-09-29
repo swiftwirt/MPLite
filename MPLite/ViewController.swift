3edaf3a98294c15cbad5a83d08e1dc3c36a3140c
swiftwirt <swiftwirt@gmail.com>
2016-09-29 22:22:44 +0300

    Initial Commit

---
+++ MPLite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MPLite
+//
+//  Created by Ivashin Dmitry on 9/29/16.
+//  Copyright © 2016 Ivashin Dmitry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
